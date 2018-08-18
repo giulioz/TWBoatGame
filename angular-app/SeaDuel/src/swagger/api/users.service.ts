e2219966865cf83af21be5562628260bac2b6ba9
giulioz <mail.zausa.giulio@gmail.com>
2018-08-18 03:04:07 +0200

    wip

--- angular-app/SeaDuel/src/swagger/api/users.service.ts
+++ angular-app/SeaDuel/src/swagger/api/users.service.ts
@@ -67,6 +67,61 @@ export class UsersService {
 
   /**
    *
+   * Get the users in contact with the logged in user
+   * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
+   * @param reportProgress flag to report request and response progress.
+   */
+  public usersByIdIdContactsGet(
+    observe?: "body",
+    reportProgress?: boolean
+  ): Observable<Array<User>>;
+  public usersByIdIdContactsGet(
+    observe?: "response",
+    reportProgress?: boolean
+  ): Observable<HttpResponse<Array<User>>>;
+  public usersByIdIdContactsGet(
+    observe?: "events",
+    reportProgress?: boolean
+  ): Observable<HttpEvent<Array<User>>>;
+  public usersByIdIdContactsGet(
+    observe: any = "body",
+    reportProgress: boolean = false
+  ): Observable<any> {
+    let headers = this.defaultHeaders;
+
+    // authentication (Bearer) required
+    if (this.configuration.apiKeys["Authorization"]) {
+      headers = headers.set(
+        "Authorization",
+        this.configuration.apiKeys["Authorization"]
+      );
+    }
+
+    // to determine the Accept header
+    const httpHeaderAccepts: string[] = ["application/json"];
+    const httpHeaderAcceptSelected:
+      | string
+      | undefined = this.configuration.selectHeaderAccept(httpHeaderAccepts);
+    if (httpHeaderAcceptSelected !== undefined) {
+      headers = headers.set("Accept", httpHeaderAcceptSelected);
+    }
+
+    // to determine the Content-Type header
+    const consumes: string[] = ["application/json"];
+
+    return this.httpClient.get<Array<User>>(
+      `${this.basePath}/users/byId/${encodeURIComponent(String(id))}/contacts`,
+      {
+        withCredentials: this.configuration.withCredentials,
+        headers: headers,
+        observe: observe,
+        reportProgress: reportProgress
+      }
+    );
+  }
+
+  /**
+   *
    * Delete an user
    * @param id The id of the user to delete
    * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
@@ -282,6 +337,124 @@ export class UsersService {
 
   /**
    *
+   * Find user by the start of user id
+   * @param id The start of the user id to find
+   * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
+   * @param reportProgress flag to report request and response progress.
+   */
+  public usersFindIdIdGet(
+    id: string,
+    observe?: "body",
+    reportProgress?: boolean
+  ): Observable<Array<User>>;
+  public usersFindIdIdGet(
+    id: string,
+    observe?: "response",
+    reportProgress?: boolean
+  ): Observable<HttpResponse<Array<User>>>;
+  public usersFindIdIdGet(
+    id: string,
+    observe?: "events",
+    reportProgress?: boolean
+  ): Observable<HttpEvent<Array<User>>>;
+  public usersFindIdIdGet(
+    id: string,
+    observe: any = "body",
+    reportProgress: boolean = false
+  ): Observable<any> {
+    if (id === null || id === undefined) {
+      throw new Error(
+        "Required parameter id was null or undefined when calling usersFindIdIdGet."
+      );
+    }
+
+    let headers = this.defaultHeaders;
+
+    // authentication (Bearer) required
+    if (this.configuration.apiKeys["Authorization"]) {
+      headers = headers.set(
+        "Authorization",
+        this.configuration.apiKeys["Authorization"]
+      );
+    }
+
+    // to determine the Accept header
+    const httpHeaderAccepts: string[] = ["application/json"];
+    const httpHeaderAcceptSelected:
+      | string
+      | undefined = this.configuration.selectHeaderAccept(httpHeaderAccepts);
+    if (httpHeaderAcceptSelected !== undefined) {
+      headers = headers.set("Accept", httpHeaderAcceptSelected);
+    }
+
+    // to determine the Content-Type header
+    const consumes: string[] = ["application/json"];
+
+    return this.httpClient.get<Array<User>>(
+      `${this.basePath}/users/findId/${encodeURIComponent(String(id))}`,
+      {
+        withCredentials: this.configuration.withCredentials,
+        headers: headers,
+        observe: observe,
+        reportProgress: reportProgress
+      }
+    );
+  }
+
+  /**
+   *
+   * Get the list of all the users
+   * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
+   * @param reportProgress flag to report request and response progress.
+   */
+  public usersGet(
+    observe?: "body",
+    reportProgress?: boolean
+  ): Observable<Array<User>>;
+  public usersGet(
+    observe?: "response",
+    reportProgress?: boolean
+  ): Observable<HttpResponse<Array<User>>>;
+  public usersGet(
+    observe?: "events",
+    reportProgress?: boolean
+  ): Observable<HttpEvent<Array<User>>>;
+  public usersGet(
+    observe: any = "body",
+    reportProgress: boolean = false
+  ): Observable<any> {
+    let headers = this.defaultHeaders;
+
+    // authentication (Bearer) required
+    if (this.configuration.apiKeys["Authorization"]) {
+      headers = headers.set(
+        "Authorization",
+        this.configuration.apiKeys["Authorization"]
+      );
+    }
+
+    // to determine the Accept header
+    const httpHeaderAccepts: string[] = ["application/json"];
+    const httpHeaderAcceptSelected:
+      | string
+      | undefined = this.configuration.selectHeaderAccept(httpHeaderAccepts);
+    if (httpHeaderAcceptSelected !== undefined) {
+      headers = headers.set("Accept", httpHeaderAcceptSelected);
+    }
+
+    // to determine the Content-Type header
+    const consumes: string[] = ["application/json"];
+
+    return this.httpClient.get<Array<User>>(`${this.basePath}/users`, {
+      withCredentials: this.configuration.withCredentials,
+      headers: headers,
+      observe: observe,
+      reportProgress: reportProgress
+    });
+  }
+
+  /**
+   *
    * Create a new user
    * @param body The user to create
    * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
@@ -334,6 +507,110 @@ export class UsersService {
     }
 
     return this.httpClient.post<any>(`${this.basePath}/users`, body, {
+      withCredentials: this.configuration.withCredentials,
+      headers: headers,
+      observe: observe,
+      reportProgress: reportProgress
+    });
+  }
+
+  /**
+   *
+   * Get the top 10 users
+   * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
+   * @param reportProgress flag to report request and response progress.
+   */
+  public usersTopGet(
+    observe?: "body",
+    reportProgress?: boolean
+  ): Observable<Array<User>>;
+  public usersTopGet(
+    observe?: "response",
+    reportProgress?: boolean
+  ): Observable<HttpResponse<Array<User>>>;
+  public usersTopGet(
+    observe?: "events",
+    reportProgress?: boolean
+  ): Observable<HttpEvent<Array<User>>>;
+  public usersTopGet(
+    observe: any = "body",
+    reportProgress: boolean = false
+  ): Observable<any> {
+    let headers = this.defaultHeaders;
+
+    // authentication (Bearer) required
+    if (this.configuration.apiKeys["Authorization"]) {
+      headers = headers.set(
+        "Authorization",
+        this.configuration.apiKeys["Authorization"]
+      );
+    }
+
+    // to determine the Accept header
+    const httpHeaderAccepts: string[] = ["application/json"];
+    const httpHeaderAcceptSelected:
+      | string
+      | undefined = this.configuration.selectHeaderAccept(httpHeaderAccepts);
+    if (httpHeaderAcceptSelected !== undefined) {
+      headers = headers.set("Accept", httpHeaderAcceptSelected);
+    }
+
+    // to determine the Content-Type header
+    const consumes: string[] = ["application/json"];
+
+    return this.httpClient.get<Array<User>>(`${this.basePath}/users/top`, {
+      withCredentials: this.configuration.withCredentials,
+      headers: headers,
+      observe: observe,
+      reportProgress: reportProgress
+    });
+  }
+
+  /**
+   *
+   * Get random users waiting to play
+   * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
+   * @param reportProgress flag to report request and response progress.
+   */
+  public usersWaitingGet(
+    observe?: "body",
+    reportProgress?: boolean
+  ): Observable<Array<User>>;
+  public usersWaitingGet(
+    observe?: "response",
+    reportProgress?: boolean
+  ): Observable<HttpResponse<Array<User>>>;
+  public usersWaitingGet(
+    observe?: "events",
+    reportProgress?: boolean
+  ): Observable<HttpEvent<Array<User>>>;
+  public usersWaitingGet(
+    observe: any = "body",
+    reportProgress: boolean = false
+  ): Observable<any> {
+    let headers = this.defaultHeaders;
+
+    // authentication (Bearer) required
+    if (this.configuration.apiKeys["Authorization"]) {
+      headers = headers.set(
+        "Authorization",
+        this.configuration.apiKeys["Authorization"]
+      );
+    }
+
+    // to determine the Accept header
+    const httpHeaderAccepts: string[] = ["application/json"];
+    const httpHeaderAcceptSelected:
+      | string
+      | undefined = this.configuration.selectHeaderAccept(httpHeaderAccepts);
+    if (httpHeaderAcceptSelected !== undefined) {
+      headers = headers.set("Accept", httpHeaderAcceptSelected);
+    }
+
+    // to determine the Content-Type header
+    const consumes: string[] = ["application/json"];
+
+    return this.httpClient.get<Array<User>>(`${this.basePath}/users/waiting`, {
       withCredentials: this.configuration.withCredentials,
       headers: headers,
       observe: observe,
