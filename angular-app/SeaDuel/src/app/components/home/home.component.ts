3d321d593538578f5e74dc0e4057b99cb39f4611
giulioz <mail.zausa.giulio@gmail.com>
2018-08-20 01:38:25 +0200

    wip

--- angular-app/SeaDuel/src/app/components/home/home.component.ts
+++ angular-app/SeaDuel/src/app/components/home/home.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-  }
+    const video = document.getElementsByTagName("video")[0];
+    video.height = document.getElementsByClassName(
+      "splash-container"
+    )[0].clientHeight;
+    video.play();
 
+    const videoCarousel = document.getElementsByClassName(
+      "carousel-back"
+    )[0] as HTMLVideoElement;
+    videoCarousel.style.top =
+      document
+        .getElementsByClassName("carousel-container")[0]
+        .getBoundingClientRect().top -
+      100 +
+      "px";
+    videoCarousel.play();
+  }
 }
