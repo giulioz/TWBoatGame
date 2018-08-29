02aced416181b548313ccc2ea0b33881cb843c99
giulioz <mail.zausa.giulio@gmail.com>
2018-08-29 17:05:26 +0200

    responsive

--- angular-app/SeaDuel/src/app/components/wave-background/wave-background.component.ts
+++ angular-app/SeaDuel/src/app/components/wave-background/wave-background.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-wave-background',
-  templateUrl: './wave-background.component.html',
-  styleUrls: ['./wave-background.component.css']
+  selector: "app-wave-background",
+  templateUrl: "./wave-background.component.html",
+  styleUrls: ["./wave-background.component.css"]
 })
 export class WaveBackgroundComponent implements OnInit {
+  @Input()
+  height?: number;
+  @Input()
+  strong: boolean;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    if (this.height) {
+      document.getElementById("water").style.transform =
+        "translate(0" + "," + this.height + ")";
+    }
   }
-
 }
